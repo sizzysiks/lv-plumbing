3ea477ae0c7a22724f286b4c1b2620ac6d542546
Antino Kila <87396903+sizzysiks@users.noreply.github.com>
2024-05-01 12:56:12 +0100

    Add files via upload

---
+++ assets/BookMessage-D078-NqR.js
@@ -0,0 +1,1 @@
+import{j as s,L as e}from"./index-39SIT71M.js";function a(){return s.jsx("div",{className:"book-message",children:s.jsxs("div",{className:"book-message-content",children:[s.jsx("h1",{children:"GET A FREE QUOTE"}),s.jsx(e,{className:"secondary-btn",to:"/contact",children:"Contact us"})]})})}export{a as B};
